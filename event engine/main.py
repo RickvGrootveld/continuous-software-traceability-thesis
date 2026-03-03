108cc36613b6581c5e8f6da0159973e75923c9be
RickvGrootveld <r.j.vangrootveld@students.uu.nl>
2026-03-03 20:14:42 +0100

    folder setup

---
+++ event engine/main.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def read_dataset():
+    pass
+
+def send_record():
+    pass
+
+if __name__ == "__main__":
+    #read_dataset()
+    #send_record()
+    # send a record from the dataset to the knowledge graph using kafka
+    pass
